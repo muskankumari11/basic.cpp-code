0cb166ff3aec763e15c80ec4e804b7346da0b46c
muskankumari11 <111870054+muskankumari11@users.noreply.github.com>
2022-10-01 19:25:51 +0530

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,15 @@
+//forloop
+//onlinecompiler
+//sumoftennaturalnumber
+#include<iostream>
+using namespace std;
+int main()
+{
+	int sum=0;
+	for(int i=0;i<=10;i++)
+	{
+		cout<<i<<" ";
+	sum=sum+i;
+	}
+	cout<<sum;
+}
