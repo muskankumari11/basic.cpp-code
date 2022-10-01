142c75776d75a128e94fd57f1dd3f798158acf85
muskankumari11 <111870054+muskankumari11@users.noreply.github.com>
2022-10-02 00:42:42 +0530

    Create forloop.cpp

---
+++ forloop.cpp
@@ -0,0 +1,18 @@
+//factorial
+//online compiler
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i,n;
+	int fact=1;
+	for(int i=1;i<=n;i++)
+	{
+		cin>>n;
+	fact=fact*i;
+	}
+	cout<<fact;
+	
+	// your code goes here
+	return 0;
+}
