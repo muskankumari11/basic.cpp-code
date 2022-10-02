72861fd892de63fb9766a27c1219387eb8929390
muskankumari11 <111870054+muskankumari11@users.noreply.github.com>
2022-10-02 15:35:36 +0530

    Create function.cpp

---
+++ function.cpp
@@ -0,0 +1,20 @@
+//onlilnr compiler
+//sumoftwonumbyusingfunction
+#include <iostream>
+using namespace std;
+int addtwo(int a,int b)
+{
+return(a+b);
+}
+int main() {
+	int num1,num2;
+	cin>>num1;
+	cin>>num2;
+//	cout<<num1;
+	//	cout<<num2;
+	int sum;
+	sum=num1+num2;
+	cout<<sum;
+	// your code goes here
+	return 0;
+}
