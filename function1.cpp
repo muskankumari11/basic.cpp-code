30f5806ae83033b054a3cba76619e483d7f217d2
muskankumari11 <111870054+muskankumari11@users.noreply.github.com>
2022-10-02 15:40:46 +0530

    Create function1.cpp

---
+++ function1.cpp
@@ -0,0 +1,18 @@
+//onlinecompiler
+//productoftwonumber
+#include <iostream>
+using namespace std;
+int multiplytwo(int a,int b)
+{
+return(a*b);
+}
+int main() {
+	int num1,num2;
+	cin>>num1;
+	cin>>num2;
+	int multiply;
+	multiply=num1*num2;
+	cout<<multiply;
+	// your code goes here
+	return 0;
+}
