76edbd03cbfe3114043da7fcb8b5c7c00bdd534c
muskankumari11 <111870054+muskankumari11@users.noreply.github.com>
2022-10-15 23:45:03 +0530

    Create list 2.cpp

---
+++ basoc.cpp-code/list 2.cpp
@@ -0,0 +1,24 @@
+//online compiler
+#include <iostream>
+#include<list>
+using namespace std;
+
+int main() {
+	list<int>l;
+l.push_back(1);
+l.push_front(3);
+//print
+for(int i:l)
+{
+	cout<<i<<" ";
+}
+l.erase(l.begin());
+cout<<"after erase"<<endl;
+for(int i:l)
+{
+	cout<<i<<" ";
+}
+
+	// your code goes here
+	return 0;
+}
