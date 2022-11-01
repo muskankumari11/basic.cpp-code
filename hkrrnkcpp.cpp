5b92b660b0117c41ed8eb7c2e1bb909ccaae5974
muskankumari11 <111870054+muskankumari11@users.noreply.github.com>
2022-11-01 22:05:42 +0530

    Create hkrrnkcpp.cpp

---
+++ hkrrnkcpp.cpp
@@ -0,0 +1,23 @@
+//inputoutput
+//hackerrank
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a,b,c;
+    int n;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    int sum;
+    sum+=a+b+c;
+        cout<<sum;
+    
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
