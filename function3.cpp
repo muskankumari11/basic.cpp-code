7a095efaf916aa34e42c33169aa9840d96a3126a
muskankumari11 <111870054+muskankumari11@users.noreply.github.com>
2022-11-01 18:26:40 +0530

    Create function3.cpp

---
+++ function3.cpp
@@ -0,0 +1,16 @@
+//online compiler
+//write a fun that return the product of two number
+#include <iostream>
+using namespace std;
+int product(int a,int b){
+	return a*b;
+}
+int main() {
+	int a,b;
+	cin>>a>>b;
+	int ans;
+	ans=a*b;
+	cout<<ans;
+	// your code goes here
+	return 0;
+}
