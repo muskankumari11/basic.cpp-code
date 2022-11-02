cb1aafe04ac897f3e718aaf4c92430c9e4d04c66
muskankumari11 <111870054+muskankumari11@users.noreply.github.com>
2022-11-02 22:46:09 +0530

    Create string6.cpp

---
+++ string6.cpp
@@ -0,0 +1,14 @@
+//online compiler
+//empty function
+#include <iostream>
+#include<string>
+using namespace std;
+int main() {
+	string s1="abc";
+	s1.clear();
+	if(s1.empty())
+	cout<<"string is empty";
+return 0;
+}
+//output is 
+//string is empty
