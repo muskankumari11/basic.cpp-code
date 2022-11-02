34c2edf4b45780d22be38974f43ff4813f4f5e01
muskankumari11 <111870054+muskankumari11@users.noreply.github.com>
2022-11-02 23:09:24 +0530

    Create string7.cpp

---
+++ string7.cpp
@@ -0,0 +1,12 @@
+//online compiler
+//erase
+//append
+#include <iostream>
+#include<string>
+using namespace std;
+int main() {
+	string s1="whatisyourname";
+	s1.erase(2,5);
+	cout<<s1;
+return 0;
+}
