3a5e278745f87454f4023c321f8bad4b68ed770d
muskankumari11 <111870054+muskankumari11@users.noreply.github.com>
2022-11-02 23:50:52 +0530

    Create string11.cpp

---
+++ string11.cpp
@@ -0,0 +1,14 @@
+//online compiler
+//substr
+#include <iostream>
+#include<string>
+using namespace std;
+int main()
+{
+//insert
+string s1="technology";
+string s=s1.substr(6,4);
+cout<<s;
+return 0;
+}
+//output=logy
