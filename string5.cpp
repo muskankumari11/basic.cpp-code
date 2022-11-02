32548fcc0b00b3226f5f28e3d5cef5c3443a9973
muskankumari11 <111870054+muskankumari11@users.noreply.github.com>
2022-11-02 22:28:50 +0530

    Create string5.cpp

---
+++ string5.cpp
@@ -0,0 +1,13 @@
+//clear
+//online compiler
+#include <iostream>
+#include<string>
+using namespace std;
+int main() {
+string s1="fam";
+string s2="ily";
+s1=s1+s2;
+//cout<<s1<<endl;
+s1.clear();
+return 0;
+}
